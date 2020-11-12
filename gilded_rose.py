48ed1473fe8569216bbaf25b9f29bf065aee79c2
Andy Dirnberger <andy.dirnberger@gmail.com>
2020-11-11 21:28:32 -0500

    Add separate tick for Aged Brie

--- gilded_rose.py
+++ gilded_rose.py
@@ -7,9 +7,11 @@ class GildedRose:
     quality: int
     days_remaining: int
 
-    def tick(self) -> None:
+    def tick(self) -> None:  # NOQA: C901
         if self.name == "normal":
             return self.normal_tick()
+        elif self.name == "Aged Brie":
+            return self.brie_tick()
 
         if (
             self.name != "Aged Brie"
@@ -50,3 +52,14 @@ class GildedRose:
         self.quality -= 1
         if self.days_remaining <= 0:
             self.quality -= 1
+
+    def brie_tick(self) -> None:
+        self.days_remaining -= 1
+        if self.quality >= 50:
+            return
+
+        self.quality += 1
+        if self.days_remaining <= 0:
+            self.quality += 1
+
+        self.quality = min(self.quality, 50)
