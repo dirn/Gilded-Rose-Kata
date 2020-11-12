bf0f0e2a5e09f35dd5c821d31d05f5da3f19f0be
Andy Dirnberger <andy.dirnberger@gmail.com>
2020-11-12 15:26:37 -0500

    Add a Normal class
    NOTE: The signature of `GildedRose` is changing. It should end up back
    at the original one once this refactor is done.

--- gilded_rose.py
+++ gilded_rose.py
@@ -2,10 +2,45 @@ from dataclasses import dataclass
 
 
 @dataclass
-class GildedRose:
-    name: str
+class Normal:
     quality: int
     days_remaining: int
+
+    def tick(self) -> None:
+        self.days_remaining -= 1
+        if self.quality == 0:
+            return
+
+        self.quality -= 1
+        if self.days_remaining <= 0:
+            self.quality -= 1
+
+
+@dataclass
+class GildedRose:
+    name: str
+    _quality: int
+    _days_remaining: int
+
+    @property
+    def quality(self) -> int:
+        if hasattr(self, "item"):
+            return self.item.quality
+        return self._quality
+
+    @quality.setter
+    def quality(self, value: int) -> None:
+        self._quality = value
+
+    @property
+    def days_remaining(self) -> int:
+        if hasattr(self, "item"):
+            return self.item.days_remaining
+        return self._days_remaining
+
+    @days_remaining.setter
+    def days_remaining(self, value: int) -> None:
+        self._days_remaining = value
 
     def tick(self) -> None:
         if self.name == "normal":
@@ -18,13 +53,8 @@ class GildedRose:
             return self.backstage_tick()
 
     def normal_tick(self) -> None:
-        self.days_remaining -= 1
-        if self.quality == 0:
-            return
-
-        self.quality -= 1
-        if self.days_remaining <= 0:
-            self.quality -= 1
+        self.item = Normal(self.quality, self.days_remaining)
+        return self.item.tick()
 
     def brie_tick(self) -> None:
         self.days_remaining -= 1
