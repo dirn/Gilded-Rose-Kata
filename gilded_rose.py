cfd3083c790fb9a0dd6637d06a180a95861f4621
Andy Dirnberger <andy.dirnberger@gmail.com>
2020-11-12 12:55:54 -0500

    Remove original (now unused) tick logic

--- gilded_rose.py
+++ gilded_rose.py
@@ -7,7 +7,7 @@ class GildedRose:
     quality: int
     days_remaining: int
 
-    def tick(self) -> None:  # NOQA: C901
+    def tick(self) -> None:
         if self.name == "normal":
             return self.normal_tick()
         elif self.name == "Aged Brie":
@@ -16,37 +16,6 @@ class GildedRose:
             return self.sulfuras_tick()
         elif self.name == "Backstage passes to a TAFKAL80ETC concert":
             return self.backstage_tick()
-
-        if (
-            self.name != "Aged Brie"
-            and self.name != "Backstage passes to a TAFKAL80ETC concert"
-        ):
-            if self.quality > 0:
-                if self.name != "Sulfuras, Hand of Ragnaros":
-                    self.quality -= 1
-        else:
-            if self.quality < 50:
-                self.quality += 1
-                if self.name == "Backstage passes to a TAFKAL80ETC concert":
-                    if self.days_remaining < 11:
-                        if self.quality < 50:
-                            self.quality += 1
-                    if self.days_remaining < 6:
-                        if self.quality < 50:
-                            self.quality += 1
-        if self.name != "Sulfuras, Hand of Ragnaros":
-            self.days_remaining -= 1
-        if self.days_remaining < 0:
-            if self.name != "Aged Brie":
-                if self.name != "Backstage passes to a TAFKAL80ETC concert":
-                    if self.quality > 0:
-                        if self.name != "Sulfuras, Hand of Ragnaros":
-                            self.quality -= 1
-                else:
-                    self.quality = self.quality - self.quality
-            else:
-                if self.quality < 50:
-                    self.quality += 1
 
     def normal_tick(self) -> None:
         self.days_remaining -= 1
