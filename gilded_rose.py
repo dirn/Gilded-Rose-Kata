fc45cfd60226e730fdece3c92432f2c2fc0eb6cc
Andy Dirnberger <andy.dirnberger@gmail.com>
2020-11-11 23:01:44 -0500

    Add separate tick for Backstage passes

--- gilded_rose.py
+++ gilded_rose.py
@@ -14,6 +14,8 @@ class GildedRose:
             return self.brie_tick()
         elif self.name == "Sulfuras, Hand of Ragnaros":
             return self.sulfuras_tick()
+        elif self.name == "Backstage passes to a TAFKAL80ETC concert":
+            return self.backstage_tick()
 
         if (
             self.name != "Aged Brie"
@@ -68,3 +70,19 @@ class GildedRose:
 
     def sulfuras_tick(self) -> None:
         ...
+
+    def backstage_tick(self) -> None:
+        self.days_remaining -= 1
+        if self.days_remaining < 0:
+            self.quality = 0
+            return
+        if self.quality >= 50:
+            return
+
+        self.quality += 1
+        if self.days_remaining < 10:
+            self.quality += 1
+        if self.days_remaining < 5:
+            self.quality += 1
+
+        self.quality = min(self.quality, 50)
